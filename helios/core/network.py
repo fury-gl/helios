4f28e533eff37a2195ebaf408647cb5449671c99
Filipi Nascimento Silva <filipinascimento@gmail.com>
2021-06-03 23:08:08 -0400

    Added network representation abstract class

---
+++ helios/core/network.py
@@ -0,0 +1,40 @@
+from abc import ABC, abstractmethod
+import numpy as np
+
+class Network(ABC):
+    @abstractmethod
+    def vertex_count():
+        ...
+    @abstractmethod
+    def edge_count():
+        ...
+    @abstractmethod
+    def edges():
+        ...
+    @abstractmethod
+    def vertices():
+        ...
+    @abstractmethod
+    def neighbors_of_vertex():
+        ...
+    @abstractmethod
+    def edge_properties():
+        ...
+    @abstractmethod
+    def vertex_properties():
+        ...
+    @abstractmethod
+    def vertex_properties():
+        ...
+    @abstractmethod
+    def add_edges():
+        ...
+    @abstractmethod
+    def add_vertices():
+        ...
+    @abstractmethod
+    def delete_edges():
+        ...
+    @abstractmethod
+    def delete_vertices():
+        ...
