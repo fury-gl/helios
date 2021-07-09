0ee602e00587b3c746b8bb8d1fe7784b8c74e320
devmessias <messias.physics@gmail.com>
2021-07-09 14:31:08 -0300

    export ForceAtlas2 to layout module

--- helios/layouts/__init__.py
+++ helios/layouts/__init__.py
@@ -1,4 +1,5 @@
 from helios.layouts.force_directed import HeliosFr
 from helios.layouts.mde import MDE
+from helios.layouts.forceatlas2gpu import ForceAtlas2
 
-__all__ = ['HeliosFr', 'MDE']
+__all__ = ['HeliosFr', 'MDE', 'ForceAtlas2']
