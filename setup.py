03386ec7eb859c169202a95ffa9c0a162bb057e5
Filipi Nascimento Silva <filipinascimento@gmail.com>
2021-10-16 14:56:24 -0400

    Version bumped to 0.3

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="helios",
-    version="0.2.2",
-    author="Filipi N. Silva",
-    author_email="filipi@iu.edu",
+    version="0.3.0",
+    author="Filipi N. Silva, Bruno Messias",
+    author_email="filipi@iu.edu, messias.physics@gmail.com",
     description="Python library to work with complex networks",
     long_description=long_description,
     long_description_content_type="text/markdown",
